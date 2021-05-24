c7bfcc26c87cf3eb3614b15fc8cbd623357448cc
Martin Spassov <mspassov@uwo.ca>
2021-05-24 19:51:26 -0400

    4 event types added

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import { Icon } from '@iconify/react'
-import locationIcon from '@iconify/icons-mdi/fire'
+import fireIcon from '@iconify/icons-mdi/fire'
+import flashIcon from '@iconify/icons-mdi/flash-alert-outline';
+import volcanoIcon from '@iconify-icons/emojione/volcano';
+import snowflakeIcon from '@iconify/icons-mdi/snowflake';
 
-const LocationMarker = ({lat, lng, onClick}) => {
+
+const LocationMarker = ({lat, lng, eventType, onClick}) => {
+
+    let iconType = null;
+    let iconStyling = '';
+    if(eventType === 'wildfire'){
+        iconType = fireIcon;
+        iconStyling = 'wildfire-icon';
+    }
+    else if(eventType === 'storm'){
+        iconType = flashIcon;
+        iconStyling = 'storm-icon';
+    }
+    else if(eventType === 'volcano'){
+        iconType = volcanoIcon;
+        iconStyling = 'volcano-icon';
+    }
+    else if(eventType === 'ice'){
+        iconType = snowflakeIcon;
+        iconStyling = 'ice-icon';
+    }   
+
     return (
         <div className='location-marker' onClick={onClick}>
-            <Icon icon={locationIcon} className='location-icon' />
+            <Icon icon={iconType} className={iconStyling} />
         </div>
     )
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -10,15 +10,36 @@ const Map = ({eventData, centerPoint, zoomLevel}) => {
     const handleClick = (id, title) =>{
         setLocationInfo({id: id, title: title});
         let box = document.querySelector('.location-info');
-        box.style.display = 'block';
+        if(box != null){
+            box.style.display = 'block';
+        }
     }
 
     const markers = eventData.map((curr) =>{
-        if(curr.categories[0].id === 8){
+        if(curr.categories[0].id === 8){    //wildfire
             const lat = curr.geometries[0].coordinates[1];
             const lng = curr.geometries[0].coordinates[0];
-            return <LocationMarker lat={lat} lng={lng} onClick={() => handleClick(curr.id, curr.title )}/>
+            return <LocationMarker eventType='wildfire' lat={lat} lng={lng} onClick={() => handleClick(curr.id, curr.title )}/>
         }
+        if(curr.categories[0].id === 10){   //severe storm 
+            const lat = curr.geometries[0].coordinates[1];
+            const lng = curr.geometries[0].coordinates[0];
+            return <LocationMarker eventType='storm' lat={lat} lng={lng} onClick={() => handleClick(curr.id, curr.title )}/>
+        }
+        if(curr.categories[0].id === 12){   //volcano
+            const lat = curr.geometries[0].coordinates[1];
+            const lng = curr.geometries[0].coordinates[0];
+            console.log(lat, lng);
+            if(typeof lat == 'number' && typeof lng == 'number'){
+                return <LocationMarker eventType='volcano' lat={lat} lng={lng} onClick={() => handleClick(curr.id, curr.title )}/>
+            }  
+        }
+        if(curr.categories[0].id === 15){   //sea and lake ice 
+            const lat = curr.geometries[0].coordinates[1];
+            const lng = curr.geometries[0].coordinates[0];
+            return <LocationMarker eventType='ice' lat={lat} lng={lng} onClick={() => handleClick(curr.id, curr.title )}/>
+        }
+        
     })
 
     return (
